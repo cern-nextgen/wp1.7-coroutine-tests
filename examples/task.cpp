d18945cdf1fb3dd85174f54d0e95f16fadbd8e0d
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-04-01 08:37:17 +0200

    add throwing example

--- examples/task.cpp
+++ examples/task.cpp
@@ -1,6 +1,7 @@
 #include "CoroutineTests/task.hpp"
 
 #include <iostream>
+#include <stdexcept>
 
 CoroutineTests::Task example() {
     std::cout << "Task resumed 0\n";
@@ -12,6 +13,17 @@ CoroutineTests::Task example() {
     std::cout << "Task resumed 3\n";
 }
 
+CoroutineTests::Task throwing_example() {
+    std::cout << "Throwing task resumed 0\n";
+    co_await std::suspend_always{};
+    std::cout << "Throwing task resumed 1\n";
+    co_await std::suspend_always{};
+    std::cout << "Throwing task resumed 2\n";
+    throw std::runtime_error("Exception!");
+    co_await std::suspend_always{};
+    std::cout << "Throwing task resumed 3\n";
+}
+
 int main() {
     std::cout << "Task example:\n";
     auto task = example();
@@ -19,5 +31,14 @@ int main() {
         task.resume();
     }
     std::cout << "Task completed\n";
+    auto another_task = throwing_example();
+    while (!another_task.done()) {
+        try {
+            another_task.resume();
+        } catch (const std::exception& e) {
+            std::cout << "Caught exception: " << e.what() << '\n';
+        }
+    }
+    std::cout << "Throwing task completed\n";
     return 0;
 }
