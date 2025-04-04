c643cfc8e13bf6356e99a5e8ad90e52f251bc3a3
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-04-04 13:22:04 +0200

    fix printout in nestable example

--- examples/nestabletask.cpp
+++ examples/nestabletask.cpp
@@ -39,7 +39,7 @@ CoroutineTests::NestableTask outer_task() {
 }
 
 int main() {
-    std::cout << "Lazy evaluation example:\n";
+    std::cout << "Nested tasks example example:\n";
     auto outer = outer_task();
     outer.resume();
     while(!outer.done()) {
