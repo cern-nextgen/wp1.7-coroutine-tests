baae21aef44dca694ae7d99ea71b02dcd525a2ca
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-04-04 13:31:09 +0200

    add example using event for synchronization

---
+++ include/CoroutineTests/event.hpp
@@ -0,0 +1,50 @@
+#include <coroutine>
+#include <deque>
+
+#include "CoroutineTests/nestabletask.hpp"
+
+// Synchronization event. Can be awaited by multiple coroutines. Can be set once
+// to resume all the coroutines suspended on it. Coroutines awaiting event that
+// was already set will resume immediately.
+class SimpleEvent {
+    public:
+    void set() {
+        if (!m_awaiter.m_ready) {
+            m_awaiter.m_ready = true;
+            for (auto& handle : m_awaiter.m_handles) {
+                if (handle && !handle.done()) {
+                    handle.resume();
+                    if (handle.promise().exception) {
+                        std::rethrow_exception(handle.promise().exception);
+                    }
+                }
+                m_awaiter.m_handles.clear();
+            }
+        }
+    }
+
+    auto& operator co_await() { return m_awaiter; }
+
+    private:
+    // If not ready stashes awaiting coroutines and return control to the
+    // caller. If ready immediately resume awaiting coroutine.
+    struct Awaiter {
+        bool await_ready() const noexcept { return false; }
+        std::coroutine_handle<> await_suspend(
+            std::coroutine_handle<CoroutineTests::NestableTask::promise_type>
+                handle) noexcept {
+            if (m_ready) {
+                return handle;
+            }
+            m_handles.push_back(handle);
+            return std::noop_coroutine();
+        }
+        void await_resume() const noexcept {}
+
+        std::deque<
+            std::coroutine_handle<CoroutineTests::NestableTask::promise_type>>
+            m_handles;
+        bool m_ready{false};
+    };
+    Awaiter m_awaiter;
+};
