4a85f67c91ef7c913bfe526651dbc70a6c38f945
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-04-07 13:51:27 +0200

    fix co_return_void typo

--- include/CoroutineTests/simplegenerator.hpp
+++ include/CoroutineTests/simplegenerator.hpp
@@ -72,7 +72,7 @@ struct SimpleGenerator<T>::promise_type {
     std::suspend_always final_suspend() const noexcept { return {}; }
     // acts as a catch block for exceptions thrown in the coroutine
     void unhandled_exception() { exception = std::current_exception(); }
-    // called on (implicit or explicit) co_return or co_return_void
+    // called on (implicit or explicit) co_return or co_return void
     void return_void() const {}
     std::suspend_always yield_value(T value) {
         m_value = std::move(value);
