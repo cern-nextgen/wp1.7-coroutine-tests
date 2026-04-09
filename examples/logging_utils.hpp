86d7c6438978496d78f8ff29c6bda58d20a5f019
Mateusz Jakub Fila <37295697+m-fila@users.noreply.github.com>
2026-04-09 16:30:34 +0200

    synchronize printouts (#64)

--- examples/logging_utils.hpp
+++ examples/logging_utils.hpp
@@ -3,14 +3,15 @@
 #include <format>
 #include <iostream>
 #include <string_view>
+#include <syncstream>
 #include <thread>
 
-static inline std::ostream& log() {
-    return std::cout << std::this_thread::get_id() << "  ";
+static inline auto log() {
+    return std::osyncstream(std::cout) << std::this_thread::get_id() << "  ";
 }
 
-static inline std::ostream& log(std::string_view self) {
-    return std::cout << std::this_thread::get_id() << "  " << self << "  ";
+static inline auto log(std::string_view self) {
+    return log() << self << "  ";
 }
 
 static inline std::string format_name(std::string_view parent,
